35d2c85e08064402d713750fe3ea499495891f3e
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-13 15:01:23 -0300

    crud-react: add skeleton components

---
+++ crud-react/frontend/src/main/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+
+export default props =>
+  <div className="App">
+    <Logo />
+    <Nav />
+    <Main />
+    <Footer />
+  </div>
