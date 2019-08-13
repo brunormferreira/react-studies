4e2ae99df62bd326101ab26daf040e88fc208f3b
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-13 16:01:56 -0300

    crud-react: footer component

--- crud-react/frontend/src/components/template/Footer.jsx
+++ crud-react/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
   <footer className="footer">
-    Rodapé
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong> Cod<span className="text-danger">3</span>r</strong>
+    </span>
   </footer>
