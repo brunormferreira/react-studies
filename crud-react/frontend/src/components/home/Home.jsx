ee42b9255a6d5a52a7357c8a899ee28e0f937817
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-14 15:43:17 -0300

    crud-react: implemented routes

--- crud-react/frontend/src/components/home/Home.jsx
+++ crud-react/frontend/src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from '../template/Main'
 
 export default props =>
-  <Main icon="home" title="Início" subtitle="Project from React.">
+  <Main icon="home" title="Início" subtitle="Projeto em React">
     <div className="display-4">Bem Vindo!</div>
     <hr />
     <p className="mb-0">Sistema para exemplificar a construção
--- crud-react/frontend/src/main/App.jsx
+++ crud-react/frontend/src/main/App.jsx
@@ -2,16 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
 export default props =>
-  <div className="app">
+  <BrowserRouter>
+    <div className="app">
     <Logo />
     <Nav />
-    <Home />
+    <Routes />
     <Footer />
-  </div>
+    </div>
+  </BrowserRouter>
