0b8a7ad22ed1e37c7c3ab9600cca586408093855
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-14 13:58:09 -0300

    crud-react: content component (usando props.children)

---
+++ crud-react/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+  <Main icon="home" title="Início" subtitle="Project from React.">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">Sistema para exemplificar a construção
+      de um cadastro desenvolvido em React!</p>
+  </Main>
--- crud-react/frontend/src/main/App.jsx
+++ crud-react/frontend/src/main/App.jsx
@@ -5,14 +5,13 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
   <div className="app">
     <Logo />
     <Nav />
-    <Main icon="home" title="Início"
-      subtitle="Project from React." />
+    <Home />
     <Footer />
   </div>
