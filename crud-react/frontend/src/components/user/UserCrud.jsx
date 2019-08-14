41afee7f7ba36d1de96d8fbb86104aa9e7621b4d
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-14 16:07:44 -0300

    crud-react: start logic crud component

--- crud-react/frontend/src/components/user/UserCrud.jsx
+++ crud-react/frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -7,7 +8,24 @@ const headerProps = {
   subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+  user: { name: '', email: ''},
+  list: []
+}
+
 export default class UserCrud extends Component {
+
+  state = { ...initialState }
+
+  clear() {
+    this.setState({ user: initialState.user })
+  }
+
+  save() {
+    
+  }
+
   render() {
     return (
       <Main {...headerProps}>
