513367b892d7b5d5acb980f99f9804df650ca3be
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-15 09:33:13 -0300

    crud-react:  logic crud component #2

--- crud-react/frontend/src/components/user/UserCrud.jsx
+++ crud-react/frontend/src/components/user/UserCrud.jsx
@@ -23,7 +23,20 @@ export default class UserCrud extends Component {
   }
 
   save() {
-    
+    const user = this.state.user
+    const method = user.id ? 'put' : 'post'
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+    axios[method](url, user)
+      .then(resp => {
+          const list = this.getUpdatedList(resp.data)
+          this.setState({ user: initialState.user, list })
+      })
+  }
+
+  getUpdatedList(user) {
+    const list = this.state.list.filter(u => u.id !== user.id)
+    list.unshift(user)
+    return list
   }
 
   render() {
