6945819243d1385f3a138c1fcb02f820e48b4678
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-15 10:29:25 -0300

    crud-react: logic crud component #3

--- crud-react/frontend/src/components/user/UserCrud.jsx
+++ crud-react/frontend/src/components/user/UserCrud.jsx
@@ -39,10 +39,61 @@ export default class UserCrud extends Component {
     return list
   }
 
+  updateField(event) {
+    const user = { ...this.state.user }
+    user[event.target.name] = event.target.value
+    this.setState({ user })
+  }
+
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+              <div className="form-group">
+                <label>Nome</label>
+                <input type="text" className="form-control"
+                    name="name"
+                    value={this.state.user.name}
+                    onChange={e => this.updateField(e)}
+                    placeholder="Digite o nome..." />
+              </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+              <div className="form-group">
+                <label>E-mail</label>
+                <input type="text" className="form-control"
+                    name="email"
+                    value={this.state.user.email}
+                    onChange={e => this.updateField(e)}
+                    placeholder="Digite o e-mail..." />
+              </div>
+          </div>
+      </div>
+
+      <hr />
+      <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button className="btn btn-primary"
+                onClick={e => this.save(e)}>
+                Salvar
+            </button>
+
+            <button className="btn btn-secondary ml-2"
+                onClick={e => this.clear(e)}>
+                Cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <Main {...headerProps}>
-       Cadastro de Usuário
+       {this.renderForm()}
       </Main>
     )
   }
