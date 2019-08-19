46d03090d19048b6b7896af20f54e01816aa9b7e
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-19 11:03:40 -0300

    add: map routes

--- todo-app/backend/src/api/todo/todoService.js
+++ todo-app/backend/src/api/todo/todoService.js
@@ -2,3 +2,5 @@ const Todo = require('./todo')
 
 Todo.methods(['get', 'post', 'put', 'delete'])
 Todo.updateOptions({ new: true, runValidators: true })
+
+module.exports = Todo
--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -10,3 +10,5 @@ server.use(bodyParser.json())
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
