09014800b28697aa9cd783672ceb8542a44de0ef
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-29 10:07:49 -0300

    add: arrow function to bind

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -15,6 +15,10 @@ class App extends Component {
     };
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
+  }
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
@@ -31,7 +35,7 @@ class App extends Component {
       <div className="App">
         <SearchBox 
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value})}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
