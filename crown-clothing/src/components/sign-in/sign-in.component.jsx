24394014b888f96efa29c2f0ee1da08e63ee0f4a
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-12 11:24:02 -0300

    add: custom buttom component

--- crown-clothing/src/components/sign-in/sign-in.component.jsx
+++ crown-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import FormInput from '../form-input/form-input.component'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -33,10 +34,24 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required />
-          <FormInput name='password' type='password' value={this.state.password} label='password' handleChange={this.handleChange} required />
+          <FormInput 
+            name='email' 
+            type='email' 
+            value={this.state.email} 
+            label='email' 
+            handleChange={this.handleChange} 
+            required 
+          />
+          <FormInput 
+            name='password' 
+            type='password' 
+            value={this.state.password} 
+            label='password' 
+            handleChange={this.handleChange} 
+            required 
+          />
 
-          <input type='submit' value='Submit Form' />
+          <CustomButton type='submit'> Sign in </CustomButton>
         </form>
       </div>
     )
