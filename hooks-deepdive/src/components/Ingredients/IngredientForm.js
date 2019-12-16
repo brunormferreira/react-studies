265088dfc6269521cc63ab6b1e57b521b6f3c5d0
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-16 20:36:59 -0300

    add: getting started with useState

--- hooks-deepdive/src/components/Ingredients/IngredientForm.js
+++ hooks-deepdive/src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const inputState = useState({title: '', amount: ''})
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +17,19 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input 
+              type="text" 
+              id="title" 
+              value={inputState[0].title}
+              onChange={event => inputState[1]({ title: event.target.value })} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input 
+              type="number" 
+              id="amount" 
+              value={inputState[0].amount}
+              onChange={event => inputState[1]({ amount: event.target.value })} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
