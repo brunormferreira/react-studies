ec336d86968fd7ab14e627da7ef5b8a7a2728c15
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-16 21:02:34 -0300

    add:  state updating

--- hooks-deepdive/src/components/Ingredients/IngredientForm.js
+++ hooks-deepdive/src/components/Ingredients/IngredientForm.js
@@ -21,7 +21,14 @@ const IngredientForm = React.memo(props => {
               type="text" 
               id="title" 
               value={inputState[0].title}
-              onChange={event => inputState[1]({ title: event.target.value })} />
+              onChange={event => {
+                const newTitle = event.target.value
+                inputState[1](prevInputState => ({ 
+                    title: newTitle,
+                    amount: prevInputState.amount
+                }))}
+              } 
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
@@ -29,7 +36,14 @@ const IngredientForm = React.memo(props => {
               type="number" 
               id="amount" 
               value={inputState[0].amount}
-              onChange={event => inputState[1]({ amount: event.target.value })} />
+              onChange={event => {
+                const newAmount = event.target.value
+                inputState[1](prevInputState => ({ 
+                    title: prevInputState.title,
+                    amount: newAmount
+                }))}
+              } 
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
