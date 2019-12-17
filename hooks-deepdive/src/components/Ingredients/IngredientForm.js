8852e22d8cc00ad48369870d49898de59173d51d
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-16 21:08:13 -0300

    add: array destructuring

--- hooks-deepdive/src/components/Ingredients/IngredientForm.js
+++ hooks-deepdive/src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({title: '', amount: ''})
+  const [inputState, setInputState] = useState({title: '', amount: ''})
 
   const submitHandler = event => {
     event.preventDefault();
@@ -20,10 +20,10 @@ const IngredientForm = React.memo(props => {
             <input 
               type="text" 
               id="title" 
-              value={inputState[0].title}
+              value={inputState.title}
               onChange={event => {
                 const newTitle = event.target.value
-                inputState[1](prevInputState => ({ 
+                setInputState(prevInputState => ({ 
                     title: newTitle,
                     amount: prevInputState.amount
                 }))}
@@ -35,10 +35,10 @@ const IngredientForm = React.memo(props => {
             <input 
               type="number" 
               id="amount" 
-              value={inputState[0].amount}
+              value={inputState.amount}
               onChange={event => {
                 const newAmount = event.target.value
-                inputState[1](prevInputState => ({ 
+                setInputState(prevInputState => ({ 
                     title: prevInputState.title,
                     amount: newAmount
                 }))}
