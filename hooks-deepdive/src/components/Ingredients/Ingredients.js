3a60d5be02c00f3b6777b6de1b0ee5bb10433f2f
Bruno Ramires <brunormferreira@hotmail.com>
2020-01-05 16:51:52 -0300

    fix: firebase link

--- hooks-deepdive/src/components/Ingredients/Ingredients.js
+++ hooks-deepdive/src/components/Ingredients/Ingredients.js
@@ -54,7 +54,7 @@ const Ingredients = () => {
 
   const addIngredientHandler = ingredient => {
     dispatchHttp({ type: 'SEND' });
-    fetch('https://react-hooks-update.firebaseio.com/ingredients.json', {
+    fetch('https://react-hooks-3a026.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
@@ -78,7 +78,7 @@ const Ingredients = () => {
   const removeIngredientHandler = ingredientId => {
     dispatchHttp({ type: 'SEND' });
     fetch(
-      `https://react-hooks-update.firebaseio.com/ingredients/${ingredientId}.json`,
+      `https://react-hooks-3a026.firebaseio.com/ingredients/${ingredientId}.json`,
       {
         method: 'DELETE'
       }
