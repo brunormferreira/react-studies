a02f30e260bfd8b0fbbceb089c0b561b6f1da7d4
Bruno Ramires <brunormferreira@hotmail.com>
2020-01-05 13:24:38 -0300

    add: implemented firebase to app

--- hooks-deepdive/src/components/Ingredients/Ingredients.js
+++ hooks-deepdive/src/components/Ingredients/Ingredients.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList'
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([])
+  const [userIngredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ])
-  }
-  const removeUserHandler = ingredientId => {
-    setUserIngredients(prevIngredients => 
-      prevIngredients.filter((ingredient) => ingredient !== ingredientId)
-      );
-  }
+    fetch('https://react-hooks-3a026.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  };
 
   return (
     <div className="App">
@@ -25,10 +36,13 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeUserHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
--- hooks-deepdive/src/components/UI/ErrorModal.js
+++ hooks-deepdive/src/components/UI/ErrorModal.js
@@ -4,7 +4,7 @@ import './ErrorModal.css';
 
 const ErrorModal = React.memo(props => {
   return (
-    <React.Fragment>
+    <>
       <div className="backdrop" onClick={props.onClose} />
       <div className="error-modal">
         <h2>An Error Occurred!</h2>
@@ -15,7 +15,7 @@ const ErrorModal = React.memo(props => {
           </button>
         </div>
       </div>
-    </React.Fragment>
+    </>
   );
 });
 
