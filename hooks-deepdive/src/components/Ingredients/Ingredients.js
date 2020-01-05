faf48e9b564ac789b3a76ea581d1f28781717079
Bruno Ramires <brunormferreira@hotmail.com>
2020-01-05 16:36:28 -0300

    add: understanding useReducer

--- hooks-deepdive/src/components/Ingredients/Ingredients.js
+++ hooks-deepdive/src/components/Ingredients/Ingredients.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there!');
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -15,7 +29,8 @@ const Ingredients = () => {
   }, [userIngredients]);
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
   const addIngredientHandler = ingredient => {
@@ -30,10 +45,14 @@ const Ingredients = () => {
         return response.json();
       })
       .then(responseData => {
-        setUserIngredients(prevIngredients => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient }
-        ]);
+        // setUserIngredients(prevIngredients => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient }
+        // ]);
+        dispatch({
+          type: 'ADD',
+          ingredient: { id: responseData.name, ...ingredient }
+        });
       });
   };
 
@@ -44,28 +63,31 @@ const Ingredients = () => {
       {
         method: 'DELETE'
       }
-    ).then(response => {
-      setIsLoading(false);
-      setUserIngredients(prevIngredients =>
-        prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-      );
-    }).catch(error => {
-      setError('Something went wrong!');
-      setIsLoading(false);
-    })
+    )
+      .then(response => {
+        setIsLoading(false);
+        // setUserIngredients(prevIngredients =>
+        //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+        // );
+        dispatch({ type: 'DELETE', id: ingredientId });
+      })
+      .catch(error => {
+        setError('Something went wrong!');
+        setIsLoading(false);
+      });
   };
 
   const clearError = () => {
     setError(null);
-  }
+  };
 
   return (
     <div className="App">
       {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
 
-      <IngredientForm 
-        onAddIngredient={addIngredientHandler} 
-        loading={isLoading} 
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
       />
 
       <section>
